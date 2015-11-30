fb9ac18b12fb59c8868471627c9a53fe31aff726
zhahang <zha_hang@163.com>
2015-12-01 00:15:12 +0800

    随便弄了下布局 加了两张默认图片

--- app/src/main/java/com/example/administrator/newtest/PlayActivity.java
+++ app/src/main/java/com/example/administrator/newtest/PlayActivity.java
@@ -1,9 +1,9 @@
 package com.example.administrator.newtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
