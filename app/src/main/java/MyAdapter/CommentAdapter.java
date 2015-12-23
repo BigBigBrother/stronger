0b4ca63b61385330979ab385cfd760efd35f91d4
zhahang <zha_hang@163.com>
2015-12-24 00:12:40 +0800

    添加点击卡片跳转到播放activity，修改PlayActivity和相应布局,添加播放界面和标题上滑隐藏（coordinatorlayout），加入评论界面

---
+++ app/src/main/java/MyAdapter/CommentAdapter.java
@@ -0,0 +1,88 @@
+package MyAdapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.administrator.newtest.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhahang on 2015/12/21.
+ */
+public class CommentAdapter extends RecyclerView.Adapter <CommentAdapter.ViewHolder>{
+
+    private List<Map<String, Object>> dataList;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imageView;
+        public TextView userName;
+        public TextView commenContent;
+        public TextView commentTime;
+        public TextView ZanCount;
+        public ImageButton imgZan;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.img);
+            userName = (TextView) itemView.findViewById(R.id.comment_user_name);
+            commenContent = (TextView) itemView.findViewById(R.id.comment_content);
+            commentTime = (TextView) itemView.findViewById(R.id.comment_time);
+            ZanCount = (TextView) itemView.findViewById(R.id.comment_zan_count);
+        }
+    }
+
+    public CommentAdapter(Context context){
+        dataList = getData();
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.imageView.setBackgroundResource((Integer) dataList.get(position).get("userimg"));
+        holder.userName.setText((String) dataList.get(position).get("username"));
+        holder.commenContent.setText((String) dataList.get(position).get("usercomment"));
+        holder.commentTime.setText((String) dataList.get(position).get("commenttime"));
+        holder.ZanCount.setText((String) dataList.get(position).get("zancount") + " 赞");
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_item ,parent ,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map;
+        for (int i = 0; i < 20; i++) {
+            map = new HashMap<String, Object>();
+            map.put("userimg", R.mipmap.ic_launcher);
+            map.put("username", "用户名:");
+            map.put("usercomment", ",用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,用户评论,");
+            map.put("commenttime", "2015-12-12");
+            map.put("zancount", i + "");
+            dataList.add(map);
+        }
+        return dataList;
+    }
+}
--- app/src/main/java/com/example/administrator/newtest/PlayActivity.java
+++ app/src/main/java/com/example/administrator/newtest/PlayActivity.java
@@ -1,15 +1,36 @@
 package com.example.administrator.newtest;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
-public class PlayActivity extends Activity {
+import MyAdapter.CommentAdapter;
+
+
+public class PlayActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_play);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.third_activity_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.back);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("back");
+        }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.play_comment_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CommentAdapter(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
     }
 }
