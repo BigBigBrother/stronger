5968ff6c28942727ca139adbee040c1c3ee53163
huangan <1274246874@qq.com>
2015-12-28 23:46:52 +0800

    增加settingActivity

--- app/src/main/java/com/example/administrator/newtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/newtest/MainActivity.java
@@ -119,6 +119,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent=new Intent(MainActivity.this,SettingActivity.class);
+            startActivity(intent);
             return true;
         }else if (id == R.id.action_logout) {
             this.finish();
---
+++ app/src/main/java/com/example/administrator/newtest/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.newtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by huangan on 15/12/28.
+ */
+public class SettingActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comment_item);
+    }
+}
